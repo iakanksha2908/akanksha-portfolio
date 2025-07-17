64e809287c178f648ac6448c849befe997913ac7
Akanksha Pawar <pawar.ak@northeastern.edu>
2025-07-17 16:52:19 -0500

    index file updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/akanksha-portfolio/' : '/',
+  base: '/akanksha-portfolio/', // ✅ exact repo name
   plugins: [react()],
   resolve: {
     alias: {
